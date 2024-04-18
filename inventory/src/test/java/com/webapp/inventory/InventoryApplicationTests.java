353fe3caa34c79f37b7f69b09f6d080ad545b90f
Dharun prakash T <61273382+DharunprakashThangasamy@users.noreply.github.com>
2024-04-18 23:10:43 +0530

    project setup

---
+++ inventory/src/test/java/com/webapp/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
